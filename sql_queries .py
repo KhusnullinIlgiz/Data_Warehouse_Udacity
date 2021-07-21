10103f996733518fad2e1428166efe156ae8c425
KhusnullinIlgiz <ilgiz.h@inbox.ru>
2021-07-21 21:03:17 +0200

    Fix debug errors
    There was one line commented, which caused to error.

--- sql_queries .py
+++ sql_queries .py
@@ -127,7 +127,7 @@ songplay_table_insert = ("INSERT INTO songplays (start_time, user_id,\
                                        staging_events.user_agent\
                             FROM  staging_events\
                             JOIN staging_songs ON (staging_songs.title = staging_events.song AND staging_songs.artist_name = staging_events.artist)\
-                            WHERE staging_events.page = 'NextSong")
+                            WHERE staging_events.page = 'NextSong'")
 
 user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level)\
                          SELECT DISTINCT staging_events.user_id, \
@@ -171,6 +171,6 @@ time_table_insert = ("INSERT INTO time (start_time, hour, day, week, month, year
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [ songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop] #staging_events_table_drop, staging_songs_table_drop,
+drop_table_queries = [ staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop] 
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
